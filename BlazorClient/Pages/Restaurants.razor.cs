cb161e3d12bbb00b2f130504237e497c8a011874
OTonGitHub <ahmed.ifhaam.work@outlook.com>
2023-05-08 04:08:54 +0500

    Added fix for restaurant code behind bugs

--- BlazorClient/Pages/Restaurants.razor.cs
+++ BlazorClient/Pages/Restaurants.razor.cs
@@ -1,27 +1,20 @@
 using API.Models;
-using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Rendering;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace BlazorClient.Pages;
-
 
 public partial class Restaurants {
     // public Restaurants()
     // {}
-    private List<RestaurantModel> Restaurants = new();
-    [inject] private HttpClient HttpClient {get; set;}
-    [inject] private IConfiguration config {get; set;}
+    private List<RestaurantModel> restaurants = new();
+    [Inject] private HttpClient HttpClient {get; set;}
+    [Inject] private IConfiguration config {get; set;}
 
     protected override async Task OnInitializedAsync() {
         var result = await HttpClient.GetAsync(config["ApiUrl"] + "/api/restaurants");
 
         if(result.IsSuccessStatusCode) {
-            Restaurants = await result.Content.ReadFromJsonAsync<List<RestaurantModel>>();
+            restaurants = await result.Content.ReadFromJsonAsync<List<RestaurantModel>>();
         }
     }
 }
