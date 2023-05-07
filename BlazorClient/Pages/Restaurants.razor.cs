e6e3b64a6dbac21d041fb93dd34ec61bfc90cfaf
OTonGitHub <ahmed.ifhaam.work@outlook.com>
2023-05-08 04:38:03 +0500

    Fix all errors, added HTTP service

--- BlazorClient/Pages/Restaurants.razor.cs
+++ BlazorClient/Pages/Restaurants.razor.cs
@@ -1,19 +1,26 @@
-using API.Models;
+﻿using API.Models;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
 
 namespace BlazorClient.Pages;
 
-public partial class Restaurants {
-    // public Restaurants()
-    // {}
+public partial class Restaurants
+{
     private List<RestaurantModel> restaurants = new();
-    [Inject] private HttpClient HttpClient {get; set;}
-    [Inject] private IConfiguration config {get; set;}
+    [Inject] private HttpClient HttpClient { get; set; }
+    [Inject] private IConfiguration Config { get; set; }
 
-    protected override async Task OnInitializedAsync() {
-        var result = await HttpClient.GetAsync(config["ApiUrl"] + "/api/restaurants");
+    protected override async Task OnInitializedAsync()
+    {
+        var result = await HttpClient.GetAsync(Config["apiUrl"] + "/api/restaurants");
 
-        if(result.IsSuccessStatusCode) {
+        if (result.IsSuccessStatusCode)
+        {
             restaurants = await result.Content.ReadFromJsonAsync<List<RestaurantModel>>();
         }
     }
